44a462e41a71baf0e4c9b9b21ced20a5a91bb3d4
XanderLynch <xanderlynch5@gmail.com>
2026-03-09 22:55:41 -0400

    Half way to melee
    everything but scripts (im commiting so i can pull)

---
+++ Cisc226-DodgingDeath/Assets/Scripts/ScytheHingeRotator.cs
@@ -0,0 +1,12 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class HingeRotator : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        //transform.LookAt()
+    }
+}
