44d7b3154559d03946441a9e71a905f835f601d5
AhmadAnnous <ahmadannous3297@gmail.com>
2026-03-31 23:30:12 -0400

    Pause Menu + Main Menu
    Added pause menu to main game scene and started a main menu scene.

---
+++ Cisc226-DodgingDeath/Assets/Scripts/UI/PauseMenu.cs
@@ -0,0 +1,55 @@
+using Unity.VisualScripting;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static  bool gamepaused = false;
+    public GameObject pausemenu;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (gamepaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pausemenu.SetActive(false);
+        Time.timeScale = 1f;
+        gamepaused = false;
+    }
+
+    void Pause()
+    {
+        pausemenu.SetActive(true);
+        Time.timeScale = 0f;
+        gamepaused = true;
+    }
+
+    public void LoadMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void OpenInventory()
+    {
+        
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
