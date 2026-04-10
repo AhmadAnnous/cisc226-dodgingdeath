1839c89f5661ab8c2af150b4b619709df7a4ef4d
AhmadAnnous <ahmadannous3297@gmail.com>
2026-04-09 22:34:56 -0400

    Game over UI
    game over ui done

--- Cisc226-DodgingDeath/Assets/Scripts/UI/YouWin.cs
+++ Cisc226-DodgingDeath/Assets/Scripts/UI/YouWin.cs
@@ -3,7 +3,6 @@ using UnityEngine.SceneManagement;
 
 public class YouWin : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     public void LoadMenu()
     {
         Time.timeScale = 1f;
