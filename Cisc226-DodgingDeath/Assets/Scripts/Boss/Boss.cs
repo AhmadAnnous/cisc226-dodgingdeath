1567322e2bc6465c45b6267e933b8b221b48ce81
AhmadAnnous <ahmadannous3297@gmail.com>
2026-04-09 21:02:43 -0400

    You win
    Stuff done i forgor

--- Cisc226-DodgingDeath/Assets/Scripts/Boss/Boss.cs
+++ Cisc226-DodgingDeath/Assets/Scripts/Boss/Boss.cs
@@ -4,14 +4,38 @@ public class BossController : MonoBehaviour
 {
     public int health = 200;
     public bool isActive = false;
+    public GameObject winscreen;
     
     public void takeDamage(int damage)
     {
         health -= damage;
         if(health <= 0)
         {
-            GameController.youWin();
+            Destroy(gameObject);
+            isActive = false;
+            winscreen.SetActive(true);
+            Time.timeScale = 0f;
         }
+    }
+
+    void Update()
+    {
+        if (!isActive)
+        {
+            if (IsNearCamera(6f))
+        {
+            isActive = true;
+        }
+        }
+    }
+
+    bool IsNearCamera(float range)
+    {
+        Vector2 camPos = Camera.main.transform.position;
+        Vector2 enemyPos = transform.position;
+
+        return Mathf.Abs(camPos.x - enemyPos.x) <= range &&
+           Mathf.Abs(camPos.y - enemyPos.y) <= range;
     }
     
 }
